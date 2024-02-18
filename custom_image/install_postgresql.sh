d5c79d63b071dd1a9a98590c20cb4fa85cc560de
raazadarsh <raj.ada@northeastern.edu>
2024-02-18 02:09:11 -0500

    updated postgres script tp create user with su priviledges

--- custom_image/install_postgresql.sh
+++ custom_image/install_postgresql.sh
@@ -5,6 +5,12 @@ sudo dnf -y install postgresql16-server
 sudo /usr/pgsql-16/bin/postgresql-16-setup initdb
 sudo systemctl enable postgresql-16
 sudo systemctl start postgresql-16
-sudo -u postgres createuser --superuser packer
-sudo -u postgres createdb packer_db -O packer
+# sudo -u postgres createuser --superuser packer
+# sudo -u postgres createdb packer_db -O packer
 
+sudo -u postgres psql -c "SELECT version();"
+
+# Create a PostgreSQL user and database, then grant privileges
+sudo -u postgres psql -c "CREATE USER test WITH ENCRYPTED PASSWORD 'test';"
+sudo -u postgres psql -c "CREATE DATABASE test;"
+sudo -u postgres psql -c "ALTER ROLE test SUPERUSER;"
