4ad3dcd86de17ac5aea362bee939ec2a4d43d7a4
raazadarsh <raj.ada@northeastern.edu>
2024-02-19 20:25:36 -0500

    edited the env file to not escape chars

--- custom_image/replace_envs.sh
+++ custom_image/replace_envs.sh
@@ -3,12 +3,12 @@
 TMP_SERVICE_FILE="/tmp/webapp.service"
 
 # Make a copy to /tmp where we have write access
-sudo cp /etc/systemd/system/webapp.service $TMP_SERVICE_FILE
+sudo cp /etc/systemd/system/webapp.service "$TMP_SERVICE_FILE"
 
 # Replace placeholders with actual values in the temporary file
-sudo sed -i "s|\\${DB_USER}|$DB_USER|g" $TMP_SERVICE_FILE
-sudo sed -i "s|\\${DB_HOST}|$DB_HOST|g" $TMP_SERVICE_FILE
-sudo sed -i "s|\\${DB_PASSWORD}|$DB_PASSWORD|g" $TMP_SERVICE_FILE
-sudo sed -i "s|\\${DB_NAME}|$DB_NAME|g" $TMP_SERVICE_FILE
+sudo sed -i "s|\${DB_USER}|${DB_USER}|g" "$TMP_SERVICE_FILE"
+sudo sed -i "s|\${DB_HOST}|${DB_HOST}|g" "$TMP_SERVICE_FILE"
+sudo sed -i "s|\${DB_PASSWORD}|${DB_PASSWORD}|g" "$TMP_SERVICE_FILE"
+sudo sed -i "s|\${DB_NAME}|${DB_NAME}|g" "$TMP_SERVICE_FILE"
 
-sudo mv $TMP_SERVICE_FILE /etc/systemd/system/webapp.service
+sudo mv "$TMP_SERVICE_FILE" /etc/systemd/system/webapp.service
