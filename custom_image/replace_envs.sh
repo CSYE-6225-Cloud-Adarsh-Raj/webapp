b160c52b76613af4ce44d98e9e79c687fd822ef6
raazadarsh <raj.ada@northeastern.edu>
2024-02-19 19:56:52 -0500

    elevated priviledges for sed in systemd service

--- custom_image/replace_envs.sh
+++ custom_image/replace_envs.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-SERVICE_FILE="/etc/systemd/system/webapp.service"
+TMP_SERVICE_FILE="/tmp/webapp.service"
 
-# Replace placeholders with actual values
-sed -i "s|\\${DB_USER}|$DB_USER|g" $SERVICE_FILE
-sed -i "s|\\${DB_HOST}|$DB_HOST|g" $SERVICE_FILE
-sed -i "s|\\${DB_PASSWORD}|$DB_PASSWORD|g" $SERVICE_FILE
-sed -i "s|\\${DB_NAME}|$DB_NAME|g" $SERVICE_FILE
+# Make a copy to /tmp where we have write access
+sudo cp /etc/systemd/system/webapp.service $TMP_SERVICE_FILE
+
+# Replace placeholders with actual values in the temporary file
+sed -i "s|\\${DB_USER}|$DB_USER|g" $TMP_SERVICE_FILE
+sed -i "s|\\${DB_HOST}|$DB_HOST|g" $TMP_SERVICE_FILE
+sed -i "s|\\${DB_PASSWORD}|$DB_PASSWORD|g" $TMP_SERVICE_FILE
+sed -i "s|\\${DB_NAME}|$DB_NAME|g" $TMP_SERVICE_FILE
+
+sudo mv $TMP_SERVICE_FILE /etc/systemd/system/webapp.service
