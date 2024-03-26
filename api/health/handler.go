c3ebbe5ea16313a767f528182537d21c8e8bd697
raazadarsh <raj.ada@northeastern.edu>
2024-03-26 15:59:15 -0400

    code cleanup

--- api/health/handler.go
+++ api/health/handler.go
@@ -2,6 +2,7 @@ package health
 
 import (
 	"net/http"
+	"webapp/logger"
 
 	"github.com/gin-gonic/gin"
 	"gorm.io/gorm"
@@ -11,11 +12,13 @@ func HealthCheckHandler(db *gorm.DB) gin.HandlerFunc {
 	return func(c *gin.Context) {
 
 		if c.Request.ContentLength > 0 {
+			logger.Logger.Error("HealthCheckHandler() - Bad Request")
 			c.Status(http.StatusBadRequest)
 			return
 		}
 
 		if len(c.Request.URL.RawQuery) > 0 {
+			logger.Logger.Error("HealthCheckHandler() - Bad Request")
 			c.Status(http.StatusBadRequest)
 			return
 		}
@@ -24,15 +27,18 @@ func HealthCheckHandler(db *gorm.DB) gin.HandlerFunc {
 
 		postgresDB, err := db.DB()
 		if err != nil {
+			logger.Logger.Error("HealthCheckHandler() - ServiceUnavailable, cannot connect to db")
 			c.Status(http.StatusServiceUnavailable)
 			return
 		}
 
 		if err := postgresDB.Ping(); err != nil {
+			logger.Logger.Error("HealthCheckHandler() - ServiceUnavailable, cannot ping db")
 			c.Status(http.StatusServiceUnavailable)
 			return
 		}
 
+		logger.Logger.Info("HealthCheckHandler() - Database health is OK")
 		c.Status(http.StatusOK)
 	}
 }
