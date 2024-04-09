b9f14ee4c2222449229191c8452f39c81fbcd17c
raazadarsh <raj.ada@northeastern.edu>
2024-04-08 21:51:48 -0400

    updated env to use vars context 1

--- custom_image/create_new_template.sh
+++ custom_image/create_new_template.sh
@@ -5,22 +5,22 @@
 
 # Define new custom OS image (this should be set or passed into the script)
 NEW_IMAGE="$PACKER_IMAGE_NAME"
-GCP_PROJECT_ID="$GCP_PROJECT_ID"
-INSTANCE_GROUP_NAME="$INSTANCE_GROUP_NAME"
+GCP_PROJECT_ID=${{ vars.GCP_PROJECT_ID }}
+INSTANCE_GROUP_NAME=${{ vars.INSTANCE_GROUP_NAME }}
 
 # Extracting information from the JSON file
-MACHINE_TYPE="$MACHINE_TYPE"
-DISK_SIZE_GB="$DISK_SIZE_GB"
-DISK_TYPE="$DISK_TYPE"
+MACHINE_TYPE=${{ vars.MACHINE_TYPE }}
+DISK_SIZE_GB=${{ vars.DISK_SIZE_GB }}
+DISK_TYPE= ${{ vars.DISK_TYPE }}
 #DISK_INTF_TYPE=$(jq -r '.properties.disks[0].interface' "$CONFIG_JSON")
 
 #NETWORK=$(jq -r '.properties.networkInterfaces[0].network' "$CONFIG_JSON" | awk -F'/' '{print $NF}')
 #SUBNET=$(jq -r '.properties.networkInterfaces[0].subnetwork' "$CONFIG_JSON" | awk -F'/' '{print $NF}')
-REGION="$REGION"
-NETWORK="$NETWORK"
-SUBNET="$SUBNET"
+REGION=${{ vars.REGION }}
+NETWORK=${{ vars.NETWORK }}
+SUBNET= ${{ vars.SUBNET }}
 # Assuming tags are listed under properties.tags.items in the JSON
-TAGS="$TAGS"
+TAGS=${{ vars.TAGS }}
 
 echo "$REGION"
 echo "$NETWORK"
@@ -59,8 +59,8 @@ EOF
 
 
 # Service account and scopes
-SERVICE_ACCOUNT_EMAIL="$SERVICE_ACCOUNT_EMAIL"
-SCOPES="$SCOPES"
+SERVICE_ACCOUNT_EMAIL=${{ vars.SERVICE_ACCOUNT_EMAIL }}
+SCOPES= ${{ vars.SCOPES }}
 NEW_INSTANCE_TEMPALTE="webapp-template-$(date +%Y%m%d%H%M%S)"
 # Constructing the gcloud command
 GCLOUD_CMD="gcloud compute instance-templates create $NEW_INSTANCE_TEMPALTE \
